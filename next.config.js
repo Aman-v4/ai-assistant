bdd02cb86eb0ad19d18aef3a018d188e8335bdf7
Aman <namaverma435@gmail.com>
2025-08-20 21:44:45 +0530

    next config edit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   images: {
     domains: ['openweathermap.org'],
   },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client'],
+  },
+  webpack: (config) => {
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      'bufferutil': 'commonjs bufferutil',
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig
